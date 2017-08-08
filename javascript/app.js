b42ea78af90189e88c9d634551c92ea5a2e18d9c
Sonny <ynnossence@yahoo.com>
2017-08-08 13:02:05 +0800

    initial commit

---
+++ javascript/app.js
@@ -0,0 +1,139 @@
+(function () {
+
+  var app = angular.module("todoApp", []);
+
+  var dd = new Date("2017-7-20");
+
+  app.controller("TodoController", function ($scope, $window) {
+
+    $scope.title = "My Awesome ToDo App";
+
+    $scope.saved = localStorage.getItem("savedList");
+
+    $scope.sortOrder = 'dateAdded';
+
+    // Test
+    $scope.showSel = () => {
+      console.log($scope.sortOrder);
+    }
+
+    $scope.todoLists = (localStorage.getItem("savedList") !== null ) ? JSON.parse($scope.saved) : [{
+        name: "Angular",
+        done: false,
+        dateAdded: dd.toDateString(),
+        duration: 1
+      },
+      {
+        name: "Javascript",
+        done: true,
+        dateAdded: dd.toDateString(),
+        duration: 2
+      },
+      {
+        name: "CSS",
+        done: false,
+        dateAdded: dd.toDateString(),
+        duration: 3
+      },
+      {
+        name: "HTML 5",
+        done: true,
+        dateAdded: dd.toDateString(),
+        duration: 4
+      }];
+
+    $scope.addTodo = function(){
+      $scope.todo = {
+        name: $scope.todoName,
+        done: false,
+        dateAdded: (new Date()).toDateString(),
+        duration: 2
+      };
+      $scope.todoLists.push($scope.todo);
+      $scope.todoName = "";
+      localStorage.setItem("savedList", JSON.stringify($scope.todoLists));
+      //console.log($scope.todoLists);
+    }
+
+    //console.log($scope.saved);
+    //console.log($scope.todoLists);
+
+    $scope.clearStorage = function(){
+      alert('Local Storage Cleared!');
+      localStorage.clear();
+      $window.location.reload();
+
+    }
+
+    $scope.showItems = () => {
+      console.log($scope.todoLists);
+      console.log($scope.savedArchive);
+    }
+
+    $scope.update = () => {
+      localStorage.setItem("savedList", JSON.stringify($scope.todoLists));
+      localStorage.setItem("archiveList", JSON.stringify($scope.archiveList));
+    }
+
+    $scope.remove = ($index) => {
+      ($scope.todoLists).splice($index,1);
+      $scope.update();
+      console.log($scope.todoLists);
+    }
+
+
+    // Long Cut
+    $scope.removeToo = function(todo){
+      ($scope.todoLists).splice(($scope.todoLists).indexOf(todo), 1)
+      $scope.update();
+      console.log(($scope.todoLists).indexOf(todo) + " Removed.");
+    }
+
+    $scope.remaining = (finished) => {
+      let count = 0;
+      for(var i = 0; i < $scope.todoLists.length ; i++){
+        if(typeof finished === 'undefined'){
+          if(!$scope.todoLists[i].done){
+            count++;
+          }
+        }else{
+          if($scope.todoLists[i].done){
+            count++;
+          }
+        }
+
+      }
+      return count;
+    }
+
+
+    //$scope.archiveList = ($scope.archiveList !== null)? JSON.parse(localStorage.getItem("archiveList")) : [];
+    $scope.savedArchive = localStorage.getItem("archiveList");
+    $scope.archiveList = (localStorage.getItem("archiveList") !== null)? JSON.parse($scope.savedArchive) : [];
+    $scope.archive = () => {
+      var indx = 0;
+      if($scope.remaining(true) > 0){
+        console.log("Archiving Items...");
+
+        for(var i = 0; i < $scope.todoLists.length; i++){
+          if($scope.todoLists[i].done){
+            ($scope.todoLists).splice(i,1);
+            $scope.archiveList.push($scope.todoLists[i]);
+          }
+        }
+
+        $scope.update();
+        console.log($scope.archiveList);
+
+      }else{
+        console.log($scope.remaining(true));
+        console.log("No Items to Archive.");
+      }
+    }
+
+
+
+
+  });
+
+})();
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  entry: './javascript/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader",
+      query: {
+        presets: ['es2015','stage-0']
+      }
+    }]
+  }
+};
